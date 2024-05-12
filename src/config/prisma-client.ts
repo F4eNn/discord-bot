2fae742c4f9989737533e164a5d6ff15d0c4b696
Mateusz <mateusz4k@outlook.com>
2024-05-12 20:49:49 +0200

    created prisma client

---
+++ src/config/prisma-client.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+declare global {
+	var prisma: PrismaClient;
+}
+
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient({ errorFormat: 'pretty' });
+	console.log('Production: Created DB connection.');
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient({ errorFormat: 'pretty' });
+		console.log('Development: Created DB connection.');
+	}
+	prisma = global.prisma;
+}
+
+export default prisma;
