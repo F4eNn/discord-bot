a2318d77d6e3b32102fb96fdd1272005f9b52c7e
Mateusz <mateusz4k@outlook.com>
2024-05-19 19:25:28 +0200

    temporary register global commands

--- src/commands/deploy-commands.ts
+++ src/commands/deploy-commands.ts
@@ -1,21 +1,27 @@
 import 'dotenv/config';
-import { REST, Routes, RESTPutAPIApplicationCommandsJSONBody, RESTPutAPIApplicationCommandsResult } from 'discord.js';
+import {
+    REST,
+    Routes,
+    RESTPutAPIApplicationCommandsJSONBody,
+    RESTPutAPIApplicationCommandsResult,
+    DiscordAPIError,
+} from 'discord.js';
 import chalk from 'chalk';
 import { COMMANDS } from '../config/enums';
 
 const log = console.log;
-const { DISCORD_TOKEN, DISCORD_GUILD_ID, DISCORD_CLIENT_ID } = process.env;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
 
-if (!DISCORD_TOKEN || !DISCORD_GUILD_ID || !DISCORD_CLIENT_ID) {
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
     throw new Error('Missing Eviroment variables');
 }
 
 const commands: RESTPutAPIApplicationCommandsJSONBody = [
-    { name: COMMANDS.Greeting, description: 'Reples with hey' },
+    { name: COMMANDS.Hello, description: 'Reples with Hi' },
     { name: COMMANDS.Meme, description: 'Generates a random meme' },
     {
         name: COMMANDS.Daily,
-        description: 'Adds daily points to your experiance level',
+        description: 'Adds daily points to your experience level.',
     },
 ];
 
@@ -23,11 +29,14 @@ const rest = new REST().setToken(DISCORD_TOKEN);
 
 (async () => {
     try {
-        const data = (await rest.put(Routes.applicationGuildCommands(DISCORD_CLIENT_ID, DISCORD_GUILD_ID), {
+        const data = (await rest.put(Routes.applicationCommands(DISCORD_CLIENT_ID), {
             body: commands,
         })) as RESTPutAPIApplicationCommandsResult;
         log(chalk.white.bgGreen(`Successfully register ${data.length} application (/) commands.`));
     } catch (error) {
+        if (error instanceof DiscordAPIError) {
+            return log(chalk.white.bgRed(`[ERROR] The command registration failed ${error.message}`));
+        }
         log(chalk.white.bgRed('[ERROR] The command registration failed'));
     }
 })();
