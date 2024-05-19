b62d92155b48c11daa4277b93dcd36a1289b1425
Mateusz <mateusz4k@outlook.com>
2024-05-19 19:28:13 +0200

    Creted greeting command and txt file for it

--- src/commands/handlers/greeting.ts
+++ src/commands/handlers/greeting.ts
@@ -1,5 +1,19 @@
 import { ChatInputCommandInteraction } from 'discord.js';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const dirname = fileURLToPath(import.meta.url);
+const greetingsPath = path.join(dirname, '../../../', 'txt/greetings.txt');
+
+const readGreetings = fs.readFileSync(greetingsPath, 'utf-8');
+const greetingsArr = readGreetings.split(/\r?\n/);
 
 export const handleGreetingCommand = async (interaction: ChatInputCommandInteraction) => {
     if (!interaction.isChatInputCommand()) return;
+
+    const userInvoker = interaction.user;
+    const greeting = greetingsArr[Math.floor(Math.random() * (greetingsArr.length -1))];
+     console.log(greeting)
+    interaction.reply(`${greeting} ${userInvoker}`);
 };
